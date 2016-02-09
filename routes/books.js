1473009e1c591b8d2d26f3584148485d0bbce0f9
marthaberner <marthaberner.mb@gmail.com>
2016-02-08 22:53:48 -0700

    books CRUD

---
+++ routes/books.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex');
+
+function Books() {
+  return knex('books');
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  Books().select().then(function (books) {
+    res.render('books/index', {books: books});
+  })
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('books/new');
+});
+
+router.post('/', function (req, res, next) {
+  var errors = [];
+  if(!req.body.title.trim()){errors.push("Title cannot be blank")}
+  if(!req.body.genre.trim()){errors.push("Genre cannot be blank")}
+  if(!req.body.cover_url.trim()){errors.push("Cover image cannot be blank")}
+  if(!req.body.description.trim()){errors.push("Description cannot be blank")}
+  if(errors.length){
+    res.render('books/new', { book: req.body, errors: errors })
+  } else {
+    Books().insert(req.body).then(function (results) {
+        res.redirect('/');
+    })
+  }
+})
+
+router.get('/:id/delete', function(req, res, next) {
+  Books().where('id', req.params.id).first().then(function (book) {
+    res.render('books/delete', {book: book});
+  })
+});
+
+router.post('/:id/delete', function(req, res, next) {
+  Books().where('id', req.params.id).del().then(function (book) {
+    res.redirect('/books');
+  })
+});
+
+router.get('/:id/edit', function(req, res, next) {
+  Books().where('id', req.params.id).first().then(function (book) {
+    res.render('books/edit', {book: book});
+  })
+});
+
+router.get('/:id', function(req, res, next) {
+  Books().where('id', req.params.id).first().then(function (book) {
+    res.render('books/show', {book: book});
+  })
+});
+
+router.post('/:id', function(req, res, next) {
+  var errors = [];
+  if(!req.body.title.trim()){errors.push("Title cannot be blank")}
+  if(!req.body.genre.trim()){errors.push("Genre cannot be blank")}
+  if(!req.body.cover_url.trim()){errors.push("Cover image cannot be blank")}
+  if(!req.body.description.trim()){errors.push("Description cannot be blank")}
+  if(errors.length){
+    res.render('books/edit', { book: req.body, errors: errors })
+  } else {
+    Books().where('id', req.params.id).update(req.body).then(function (results) {
+      res.redirect('/');
+    })
+  }
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('../db/knex');
 
-function Books() {
-  return knex('books');
-}
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  Books().select().then(function (books) {
-    console.log("*****");
-    console.log(books.length);
-    res.render('index', {books: books});
-  })
+  res.render('index');
 });
 
 module.exports = router;
