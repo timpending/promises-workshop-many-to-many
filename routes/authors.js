9730b0e1c6fc67eb78542008199d017a3b465ee2
Tim Pender <mrtimpender@gmail.com>
2016-08-23 15:21:32 -0600

    author show route with all books working

--- routes/authors.js
+++ routes/authors.js
@@ -84,6 +84,12 @@ router.post('/:id', function (req, res, next) {
 })
 
 router.get('/:id', function (req, res, next) {
+  helpers.getAuthorBooks(req.params.id).then(function(results){
+    res.render('authors/show', {
+      author: results.author,
+      books: results.books
+    })
+  })
   // find the author in Authors
   // get all of the authors book_ids from Authors_Books
   // get all of the authors books from BOOKs
